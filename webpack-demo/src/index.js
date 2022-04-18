28c46ed9716565e02a9b7b09e90e71129bbadc77
Sean DiGirolamo <s.digirolamo218@gmail.com>
2022-04-18 16:01:29 -0400

    Followed webpack tutorial further

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', myName('Sean')], ' ');
 
     return element;
 }
