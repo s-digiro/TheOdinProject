81acd4a65d5808909c29f4a42e61e58525e117ee
Sean DiGirolamo <s.digirolamo218@gmail.com>
2022-04-18 16:31:15 -0400

    Completed webpack asset tutorial

---
+++ webpack-demo/README.md
@@ -0,0 +1,7 @@
+https://www.theodinproject.com/lessons/node-path-javascript-es6-modules
+
+https://webpack.js.org/guides/getting-started/
+
+https://www.theodinproject.com/lessons/node-path-javascript-webpack
+
+https://webpack.js.org/guides/asset-management/
--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,35 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
+import Cat from './cat.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', myName('Sean')], ' ');
+    element.classList.add('hello');
+
+    const cat = new Image();
+    cat.src = Cat;
+
+    element.appendChild(cat);
+
+    console.log(Data);
+    console.log(Notes);
 
     return element;
 }
